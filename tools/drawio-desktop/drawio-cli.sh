8612733fb5c25433af1c94581cfbc3bb78e8b66c
datamonsterr <phamthanhdat17092004@gmail.com>
2026-06-30 02:15:16 +0700

    checkpoint 1

--- tools/drawio-desktop/drawio-cli.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-APPIMAGE="$ROOT/drawio.AppImage"
-exec "$APPIMAGE" --appimage-extract-and-run --no-sandbox "$@"
